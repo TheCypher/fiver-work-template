58ece35c63de0692e3ef8b7f61ad9f41aa62b8ad
someone <nicksonthecypher@gmail.com>
2021-11-08 12:46:34 -0800

    customerizer => index

--- src/index.js
+++ src/index.js
@@ -36,25 +36,8 @@ ReactDOM.render(
   <BrowserRouter>
     <Switch>
       <Switch>
-        <Route path="/index" render={(props) => <Index {...props} />} />
         <Route
-          path="/nucleo-icons"
-          render={(props) => <NucleoIcons {...props} />}
-        />
-        <Route
-          path="/landing-page"
-          render={(props) => <LandingPage {...props} />}
-        />
-        <Route
-          path="/profile-page"
-          render={(props) => <ProfilePage {...props} />}
-        />
-        <Route
-          path="/login-page"
-          render={(props) => <LoginPage {...props} />}
-        />
-        <Route
-          path="/customerizer"
+          path="/index"
           render={(props) => <CustomerizerPage {...props} />}
         />
         <Redirect to="/index" />
